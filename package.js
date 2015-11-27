a490563499ae6d7cf8e95f33e7dde4f60571e6b0
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-11-27 13:00:27 +0100

    chore: set meteor package

--- package.js
+++ package.js
@@ -2,12 +2,13 @@ Package.describe({
     name: 'mys:version-compare',
     version: '1.0.0',
     summary: 'Compare two versions. Convert them to integer format',
-    git: 'https://githubc.com/kamilkisiela/meteor-version-compare.git',
+    git: 'https://githubc.com/kamilkisiela/version-compare.git',
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('0.9.0');
-    api.addFiles('dist/version-compare.js');
-    api.export('versionCompare');
+    api.versionsFrom('METEOR@1.0');
+    api.use('ecmascript');
+    api.addFiles('dist/meteor-version-compare.js');
+    api.export('meteorVersionCompare');
 });
